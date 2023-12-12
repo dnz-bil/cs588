352bcc3747831c3372d4e70ab58d1a3e7de63a1d
dnz-bil <117210930+dnz-bil@users.noreply.github.com>
2023-12-12 23:57:19 +0300

    initial commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+1) prepare_dump.sh sql dump ı indirip açıyor.
+
+2) run_sql_script.sh :
+	25. satırdaki 'path_to_output_file' string'i ile csv file'ın yazılacağı path değiştirilmeli.
+	"../output_dir/output.csv" gibi
+
+3) outputun yazılacağı directory'ye okuma yazma yetkisi verilmesi gerekiyor.
+	command:"chmod u+w directory_path"
+	
+4) script çalıştığında database yaratılıp sql dump başarılı bir şekilde yüklenirse sonraki sorgular için aşağıdaki iki komut comment out yapılabilir.
+	
+	sudo -u $PG_USER createdb $DB_NAME
+	sudo -u $PG_USER psql -d $DB_NAME -f $DB_DUMP_FILE
---
+++ run_sql_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+DB_NAME="ccdb"
+DB_DUMP_FILE="./bcb.pgdump/bcb" 
+PG_USER="postgres"
+
+# Create the database
+sudo -u $PG_USER createdb $DB_NAME
+
+# Restore the database from the dump
+sudo -u $PG_USER psql -d $DB_NAME -f $DB_DUMP_FILE
+
+# Create .csv file for the given format
+# function_id_one, function_id_two, syntactic_type, internal, text_of_funciton_id_one, text_of_funciton_id_two
+# output file is 25.7 GB
+sudo -u $PG_USER psql -d $DB_NAME <<EOF
+COPY (
+  SELECT
+    function_id_one,
+    function_id_two,
+    syntactic_type,
+    internal,
+    (SELECT text FROM public.pretty_printed_functions WHERE function_id = clones.function_id_one) AS pretty_function_one,
+    (SELECT text FROM public.pretty_printed_functions WHERE function_id = clones.function_id_two) AS pretty_function_two
+  FROM public.clones LIMIT 1
+) TO 'path_to_output_file' WITH CSV HEADER;
+EOF
+
