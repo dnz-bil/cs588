cef2bdf5229ad9c5a424b7d820a1bcab613b14bc
dnz-bil <117210930+dnz-bil@users.noreply.github.com>
2023-12-16 22:46:19 +0300

    Update prepare_dump.sh
    name fix

--- prepare_dump.sh
+++ prepare_dump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 link="https://rmy0rw.bn.files.1drv.com/y4mv4s8vvt7K7WH0HgsHbbwU-5ern2laoOVJZpKioBbF8Zdz8pLBsQ7QhOeKtHriJo4Y70sASti5OgllmqEGqHE_r0mgpbArOzkigTQCrSAy14eAY-eCMBRWvv80DEX8B2FzB1VdaFjHilBQKGEEum1QyibQttrgRQmkheqijyJdGzIz3F3OfRoOO3G2tqXmx7PrqYYzVRcdbAjPid8H9W1QQ"
-wget $link -O bcb.dump.tar.gz
+wget $link -O bcb.pgdump.tar.gz
 tar -xvf bcb.pgdump.tar.gz
